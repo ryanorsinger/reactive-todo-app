139a40a4301fcbac76f667861b9629222a9278b0
Ryan Orsinger <ryan.orsinger@gmail.com>
2014-10-12 19:47:49 -0500

    Add helper to TodoItem and event listener to update data when checked.

--- todos.js
+++ todos.js
@@ -3,6 +3,7 @@
 // Create a new MongoDB collection called 'todos'
 Todos = new Meteor.Collection('todos');
 
+// Code that executes only on the client
 if (Meteor.isClient) {
     // Define a helper on the TodosPanel template
     Template.TodosPanel.helpers({
@@ -19,8 +20,26 @@ if (Meteor.isClient) {
             return this.is_done ? 'checked' : '';
         }
     });
+
+    // Call the events method of the TodoItem template
+    // Update the underlying data when user clicks the checkbox
+    Template.TodoItem.events({
+        // Events object takes the selector as the 'key' and the event handler as the 'value'.
+        // We want to respond to the click event where the name is 'done'
+        // function takes two parameters, the event object and template object
+        'click [name=is_done]': function(e, tmpl) {
+            var id = this._id;
+            var isDone = tmpl.find('input').checked;
+            Todos.update({_id: id}, {
+                $set: {
+                    is_done: isDone
+                }
+            });
+        }
+    });
 }
 
+// Code that will run only on the server
 if (Meteor.isServer) {
 
 }
