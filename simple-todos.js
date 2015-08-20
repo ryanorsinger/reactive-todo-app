02cceeea40bd238c2b5fc63104b34383b19bc2f5
ryanorsinger <ryan.orsinger@gmail.com>
2015-08-20 11:20:15 -0500

    Add helper functions and event functions in client

--- simple-todos.js
+++ simple-todos.js
@@ -1,17 +1,34 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Tasks = new Mongo.Collection("tasks");
+Tools = new Mongo.Collection("tools");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+
+if (Meteor.isClient) {
+  // Helper functions
+  Template.body.helpers({
+    tasks: function() {
+      return Tasks.find({});
+    },
+    items: function() {
+      return Tools.find({});
     }
   });
+  // Event handling
+  Template.body.events({
+    "submit .new-task": function(event) {
+      // Prevent default browser form submit.
+      event.preventDefault();
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      // Get the value from the form element.
+      var text = event.target.text.value;
+
+      // Insert the task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear the form
+      event.target.text.value = '';
     }
   });
 }
@@ -19,5 +36,8 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    console.log('I\'m running on the server, yay!');
   });
 }
+
+console.log('This code runs both on server and client');
